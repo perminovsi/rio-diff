92cfdcca978bed86bd6287bde34c29b5ff2f0a09
perminov <perminovsi@ya.ru>
2026-01-03 22:09:17 +0500

    feat: add ignore options for all checked parameters

--- main.py
+++ main.py
@@ -93,7 +93,17 @@ def raster_diff(
     base_raster: str,
     test_raster: str,
     *,
+    ignore_height: bool = False,
+    ignore_width: bool = False,
+    ignore_bands: bool = False,
+    ignore_dtype: bool = False,
+    ignore_nodata: bool = False,
+    ignore_bbox: bool = False,
+    ignore_crs: bool = False,
+    ignore_transform: bool = False,
     ignore_metadata: bool = False,
+    ignore_stats: bool = False,
+    ignore_pixel_values: bool = False,
 ) -> bool:
     is_equal = True
     if calc_hash(base_raster) == calc_hash(test_raster):
@@ -102,61 +112,69 @@ def raster_diff(
     base_props = read_raster_props(base_raster)
     test_props = read_raster_props(test_raster)
 
-    if base_props.height != test_props.height:
-        print(f"< height: {base_props.height}")
-        print("---")
-        print(f"> height: {test_props.height}")
-        print("")
-        is_equal = False
+    if not ignore_height:
+        if base_props.height != test_props.height:
+            print(f"< height: {base_props.height}")
+            print("---")
+            print(f"> height: {test_props.height}")
+            print("")
+            is_equal = False
 
-    if base_props.width != test_props.width:
-        print(f"< width: {base_props.width}")
-        print("---")
-        print(f"> width: {test_props.width}")
-        print("")
-        is_equal = False
+    if not ignore_width:
+        if base_props.width != test_props.width:
+            print(f"< width: {base_props.width}")
+            print("---")
+            print(f"> width: {test_props.width}")
+            print("")
+            is_equal = False
 
-    if base_props.bands != test_props.bands:
-        print(f"< bands: {base_props.bands}")
-        print("---")
-        print(f"> bands: {test_props.bands}")
-        print("")
-        is_equal = False
+    if not ignore_bands:
+        if base_props.bands != test_props.bands:
+            print(f"< bands: {base_props.bands}")
+            print("---")
+            print(f"> bands: {test_props.bands}")
+            print("")
+            is_equal = False
 
-    if base_props.dtype != test_props.dtype:
-        print(f"< dtype: {base_props.dtype}")
-        print("---")
-        print(f"> dtype: {test_props.dtype}")
-        print("")
-        is_equal = False
+    if not ignore_dtype:
+        if base_props.dtype != test_props.dtype:
+            print(f"< dtype: {base_props.dtype}")
+            print("---")
+            print(f"> dtype: {test_props.dtype}")
+            print("")
+            is_equal = False
 
-    if base_props.nodata != test_props.nodata:
-        print(f"< nodata: {base_props.nodata}")
-        print("---")
-        print(f"> nodata: {test_props.nodata}")
-        print("")
-        is_equal = False
+    if not ignore_nodata:
+        if base_props.nodata != test_props.nodata:
+            print(f"< nodata: {base_props.nodata}")
+            print("---")
+            print(f"> nodata: {test_props.nodata}")
+            print("")
+            is_equal = False
 
-    if base_props.bbox != test_props.bbox:
-        print(f"< bbox: {base_props.bbox}")
-        print("---")
-        print(f"> bbox: {test_props.bbox}")
-        print("")
-        is_equal = False
+    if not ignore_bbox:
+        if base_props.bbox != test_props.bbox:
+            print(f"< bbox: {base_props.bbox}")
+            print("---")
+            print(f"> bbox: {test_props.bbox}")
+            print("")
+            is_equal = False
 
-    if base_props.crs != test_props.crs:
-        print(f"< crs: {base_props.crs}")
-        print("---")
-        print(f"> crs: {test_props.crs}")
-        print("")
-        is_equal = False
+    if not ignore_crs:
+        if base_props.crs != test_props.crs:
+            print(f"< crs: {base_props.crs}")
+            print("---")
+            print(f"> crs: {test_props.crs}")
+            print("")
+            is_equal = False
 
-    if base_props.transform != test_props.transform:
-        print(f"< geotransform: {base_props.transform}")
-        print("---")
-        print(f"> geotransform: {test_props.transform}")
-        print("")
-        is_equal = False
+    if not ignore_transform:
+        if base_props.transform != test_props.transform:
+            print(f"< geotransform: {base_props.transform}")
+            print("---")
+            print(f"> geotransform: {test_props.transform}")
+            print("")
+            is_equal = False
 
     if not ignore_metadata:
         # TODO: выводить конкретно в чем разница и для какого канала
@@ -174,20 +192,22 @@ def raster_diff(
             print("")
             is_equal = False
 
-    if base_props.stats != test_props.stats:
-        # TODO: выводить детально в каких каналах и в чем различия
-        print(f"< statistics: {base_props.stats}")
-        print("---")
-        print(f"> statistics: {test_props.stats}")
-        print("")
-        is_equal = False
+    if not ignore_stats:
+        if base_props.stats != test_props.stats:
+            # TODO: выводить детально в каких каналах и в чем различия
+            print(f"< statistics: {base_props.stats}")
+            print("---")
+            print(f"> statistics: {test_props.stats}")
+            print("")
+            is_equal = False
 
-    if calc_diff(base_raster, test_raster) is False:
-        print("< pixel values: ...")  # TODO: добавить подробный вывод в чем отличия у пикселей
-        print("---")
-        print("> pixel values: ...")
-        print("")
-        is_equal = False
+    if not ignore_pixel_values:
+        if calc_diff(base_raster, test_raster) is False:
+            print("< pixel values: ...") # TODO: добавить подробный вывод в чем отличия у пикселей
+            print("---")
+            print("> pixel values: ...")
+            print("")
+            is_equal = False
 
     return is_equal
 
@@ -207,7 +227,17 @@ if __name__ == "__main__":
         "temp/icon/OLD.icon.2026010200.003.t_2m.tif",
         "temp/icon/NEW.icon.2026010200.003.t_2m.tif",
         # "temp/icon/OLD.icon.2026010200.003.td_2m.tif",
+        ignore_height=False,
+        ignore_width=False,
+        ignore_bands=False,
+        ignore_dtype=False,
+        ignore_nodata=False,
+        ignore_bbox=False,
+        ignore_crs=False,
+        ignore_transform=False,
         ignore_metadata=True,
+        ignore_stats=False,
+        ignore_pixel_values=False,
     )
 
     print(rs)
